e48ae436e4b0d55bcc8e5c8b20f9068da3955bbf
duochen13 <duochen@umich.edu>
2020-04-25 23:57:14 -0400

    add driver

--- driver.py
+++ driver.py
@@ -3,6 +3,7 @@ from PIL import Image
 import numpy as np
 from helper import print_quad_tree_iterative, print_quad_tree_recursive
 from quadtree import construct_quad_tree, convertFromTreeToMatrix
+from matplotlib import pyplot as plt
 
 test_matrix_0 = [[1,1,2,3],
                  [1,1,4,5],
@@ -28,11 +29,18 @@ img = imageio.imread('telsajoke.png')
 I,J,K = img.shape # (738 1754 4)
 N = min(I, J)
 
+
 img_layer_0, img_layer_1, img_layer_2 = img[:,:,0], img[:,:,1], img[:,:,2]
 img_layer_0 = img_layer_0[:,:N]
 img_layer_1 = img_layer_1[:,:N]
 img_layer_2 = img_layer_2[:,:N]
 
+print(img[:,:,3])
+
+
+# print(img_layer_0)
+# print(img_layer_1)
+# print(img_layer_2)
 
 err = 0
 img_layer_0_quadtree = construct_quad_tree(img_layer_0, err)
@@ -43,33 +51,35 @@ img_layer_0_recover = convertFromTreeToMatrix(img_layer_0_quadtree, N)
 img_layer_1_recover = convertFromTreeToMatrix(img_layer_1_quadtree, N)
 img_layer_2_recover = convertFromTreeToMatrix(img_layer_2_quadtree, N)
 
-created_img = [[[255 for _ in range(3)] for _ in range(N)] for _ in range(N)]
-for k in range(3):
+created_img = [[[0 for _ in range(4)] for _ in range(N)] for _ in range(N)]
+# (738, 738, 4)
+
+
+for k in range(4):
     if k == 0:
         for i in range(N):
             for j in range(N):
-                created_img[i][j][k] = img_layer_0_recover[i][j]
+                created_img[i][j][k] = img[i][j][k] # img_layer_0_recover[i][j]
     elif k == 1:
         for i in range(N):
             for j in range(N):
-                created_img[i][j][k] = img_layer_1_recover[i][j]
+                created_img[i][j][k] = img[i][j][k] # img_layer_1_recover[i][j]
     elif k == 2:
         for i in range(N):
             for j in range(N):
-                created_img[i][j][k] = img_layer_2_recover[i][j]
-    # else:
-    #     for i in range(N):
-    #         for j in range(N):
-    #             created_img[i][j][k] = 255
+                created_img[i][j][k] = img[i][j][k] # img_layer_2_recover[i][j]
+    else:
+        print(k)
+        for i in range(N):
+            for j in range(N):
+                created_img[i][j][k] = 255 #img[i][j][k]
+# print(created_img)
 
-# print("first row layer====")
-# print(img_layer_0[0][:20])
-# print("=================")
-# print(img_layer_0_recover[0][:20])
+plt.imshow(created_img, interpolation='nearest')
+plt.show()
 
-data_np = np.asarray(created_img)
-actual_img = Image.fromarray(data_np, 'RGB')
-# actual_img = Image.fromarray(np.asarray(img[:,:,:3]), 'RGB')
-actual_img.save('telsajoke_recover.png')
-actual_img.show()
+# data_np = np.asarray(created_img)
+# actual_img = Image.fromarray(data_np, 'RGB')
+# # actual_img = Image.fromarray(np.asarray(img[:,:,:3]), 'RGB')
+# actual_img.save('telsajoke_recover.png')
 
